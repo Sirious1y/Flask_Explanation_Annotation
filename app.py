5b1935f078734e82c47292ae4b737a8d3ad5d443
Sirious1y <jamess0378@gmail.com>
2024-03-06 23:34:06 -0500

    Allow user upload their own CNN model in pytorch

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, request, redirect, url_for, flash
 from forms import ReasonForm
 import os
 import pandas as pd
@@ -6,7 +6,6 @@ import json
 from datetime import datetime
 from pytz import timezone
 from config import SECRET_KEY
-from flask import request
 
 ## Run initialization code "init_code.py"
 # import init_code
@@ -19,6 +18,8 @@ from PIL import Image
 import base64
 import numpy as np
 # np.set_printoptions(threshold=np.inf)
+
+import model
 
 
 #*##########
@@ -54,111 +55,171 @@ app.config['SECRET_KEY'] = SECRET_KEY
 # tz = timezone('EST')
 
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/", methods=['GET'])
+def home():
+    return render_template('home.html')
+
+@app.route("/index", methods=['GET', 'POST'])
 def index():
-    form = ReasonForm()
-    result_path = f'output/{csvname_res}'
-    try:
-        if True:
-            df_results = pd.read_csv(result_path, index_col=0)
-            df_current = pd.read_csv(f'output/{csvname_curr}', index_col=0)
-            idx_list = df_results['img_idx']
-            current_order = int(df_current['current_order'])
-            current_idx = idx_list[current_order].split('.jpg')[0]
-            finish_text = ""
-            results = []
-            form_display = True
+    if request.method == 'POST':
+        if len(request.form['model_name']) == 0:
+            session['model_path'] = None
+        else:
+            model_path = './model/' + request.form['model_name']
+            if os.path.isfile(model_path):
+                session['model_path'] = model_path
+            else:
+                flash('Model does not exist. Please double check the name you entered. ')
+                return redirect(url_for('home'))
 
-            if form.validate_on_submit():
-                svg_path_input = request.form['crop_input']  # PNG-base64 string
-                map_all = pil2bin_matrix(svg_path_input)
+        form = ReasonForm()
+        result_path = f'output/{csvname_res}'
+        try:
+            if True:
+                df_results = pd.read_csv(result_path, index_col=0)
+                df_current = pd.read_csv(f'output/{csvname_curr}', index_col=0)
+                idx_list = df_results['img_idx']
+                current_order = int(df_current['current_order'])
+                current_idx = idx_list[current_order].split('.jpg')[0]
+                finish_text = ""
+                results = []
+                form_display = True
 
-                try:
-                    map_all = (cv2.resize(np.float32(map_all), matrix_resize_w_h) > 0) * 1  # (width, height)
-                    results.append(json.dumps(map_all.tolist()))  # narray/list to string
-                    results.append("good")
-                    results.append(str(matrix_resize_w_h))
+                img_paths = []
 
-                except:
-                    results.append(map_all)  # skipped
-                    results.append(svg_path_input)  # skipped
-                    results.append(str(matrix_resize_w_h))
+                img_paths.append(f'static/images/{folder_name}/{current_idx}.jpg')
 
-                df_results.loc[current_order, :len(results)+1] = [current_idx] + results
-                print(f'map_all: {map_all}')
-                
-                if current_order + 1 < len(idx_list):
-                    current_order += 1
-                    current_idx = idx_list[current_order].split('.jpg')[0]
+                session['img_path'] = img_paths
+
+                if form.validate_on_submit():
+                    svg_path_input = request.form['crop_input']  # PNG-base64 string
+                    map_all = pil2bin_matrix(svg_path_input)
+
+                    try:
+                        map_all = (cv2.resize(np.float32(map_all), matrix_resize_w_h) > 0) * 1  # (width, height)
+                        results.append(json.dumps(map_all.tolist()))  # narray/list to string
+                        results.append("good")
+                        results.append(str(matrix_resize_w_h))
+
+                    except:
+                        results.append(map_all)  # skipped
+                        results.append(svg_path_input)  # skipped
+                        results.append(str(matrix_resize_w_h))
+
+                    df_results.loc[current_order, :len(results)+1] = [current_idx] + results
+                    print(f'map_all: {map_all}')
+
+                    if current_order + 1 < len(idx_list):
+                        current_order += 1
+                        current_idx = idx_list[current_order].split('.jpg')[0]
+
+                    else:
+                        finish_text = f"Thank You! Your verification code is: {verify_code} (only appears once)"
+                        form_display = False
+                        os.remove(result_path)
+                        timestr = datetime.now(tz).strftime("%Y%m%d_%H%M%S_%f")[:-4]
+                        result_path = f"output/done/results_{folder_name}_{chunk_id}_{task_type}_{timestr}.csv"
+                        pass
+
+                    df_results.to_csv(result_path)
 
                 else:
-                    finish_text = f"Thank You! Your verification code is: {verify_code} (only appears once)"
-                    form_display = False
-                    os.remove(result_path)
-                    timestr = datetime.now(tz).strftime("%Y%m%d_%H%M%S_%f")[:-4]
-                    result_path = f"output/done/results_{folder_name}_{chunk_id}_{task_type}_{timestr}.csv"
                     pass
 
-                df_results.to_csv(result_path)
+                print('reached 113')
+                #### Radio default selections
 
-            else:
-                pass    
-            
-            #### Radio default selections
+                form.reason_input_1.data = 'True'
+                form.reason_input_2.data = 'True'
+                form.reason_input_3.data = 'True'
+                form.reason_input_4.data = 'True'
+                form.reason_input_5.data = 'True'
+                form.reason_input_6.data = 'True'
+                form.reason_input_7.data = 'True'
+                form.reason_input_8.data = 'True'
+                form.reason_input_9.data = '5'
+                form.reason_input_10.data = '5'
+                form.reason_input_11.data = '5'
+                form.reason_input_12.data = '5'
 
-            form.reason_input_1.data = 'True'
-            form.reason_input_2.data = 'True'
-            form.reason_input_3.data = 'True'
-            form.reason_input_4.data = 'True'
-            form.reason_input_5.data = 'True'
-            form.reason_input_6.data = 'True'
-            form.reason_input_7.data = 'True'
-            form.reason_input_8.data = 'True'
-            form.reason_input_9.data = '5'
-            form.reason_input_10.data = '5'
-            form.reason_input_11.data = '5'
-            form.reason_input_12.data = '5'
 
-            img_paths = []
+                df_current.loc[0, 'current_order'] = current_order
+                df_current.loc[0, 'current_idx'] = idx_list[current_order]
 
-            img_paths.append(f'static/images/{folder_name}/{current_idx}.jpg')
+                df_current.to_csv(f'output/{csvname_curr}')
 
-            df_current.loc[0, 'current_order'] = current_order
-            df_current.loc[0, 'current_idx'] = idx_list[current_order]
+                q1_q2_display = False
+                q3_display = False
 
-            df_current.to_csv(f'output/{csvname_curr}')
+                if q1_q2_display == False:
+                    q1_q2_display = "display: none;"
+                else:
+                    q1_q2_display = ""
 
-            q1_q2_display = False
-            q3_display = False
+                if q3_display == False:
+                    q3_display = "display: none;"
+                else:
+                    q3_display = ""
 
-            if q1_q2_display == False:
-                q1_q2_display = "display: none;"
-            else:
-                q1_q2_display = ""
+                if form_display == False:
+                    draw_display = "display: none;"
+                else:
+                    draw_display = ""
 
-            if q3_display == False:
-                q3_display = "display: none;"
-            else:
-                q3_display = ""
+                if task_type == 'factual':
+                    task_counter_display = "display: none;"
+                    task_fact_display = ""
+                else:
+                    task_counter_display = ""
+                    task_fact_display = "display: none;"
 
-            if form_display == False:
-                draw_display = "display: none;"
-            else:
-                draw_display = ""
+                task_title = folder_name.split("_")[1] + "_" + chunk_id + "_" + task_type
 
-            if task_type == 'factual':
-                task_counter_display = "display: none;"
-                task_fact_display = ""
-            else:
-                task_counter_display = ""
-                task_fact_display = "display: none;"
+                return render_template('index.html', title='Main', current_order=str(current_order+1), len_idx = len(idx_list), finish_text=finish_text, img_paths=img_paths, form=form, form_display=form_display, q1_q2_display=q1_q2_display, q3_display=q3_display, draw_display=draw_display, drawing_pad_size=drawing_pad_size, task_title=task_title, task_counter_display=task_counter_display, task_fact_display=task_fact_display)
 
-            task_title = folder_name.split("_")[1] + "_" + chunk_id + "_" + task_type
+        except:
+            return render_template('blank.html', title='Blank')
+    else:
+        flash('Invalid! Please set the model and class label.')
+        return redirect(url_for('home'))
 
-            return render_template('index.html', title='Main', current_order=str(current_order+1), len_idx = len(idx_list), finish_text=finish_text, img_paths=img_paths, form=form, form_display=form_display, q1_q2_display=q1_q2_display, q3_display=q3_display, draw_display=draw_display, drawing_pad_size=drawing_pad_size, task_title=task_title, task_counter_display=task_counter_display, task_fact_display=task_fact_display)
 
-    except:
-        return render_template('blank.html', title='Blank')
+@app.route('/result', methods=['GET', 'POST'])
+def result():
+    if request.method == 'POST':
+        svg_path_input = request.form['crop_input']  # PNG-base64 string
+        map_all = pil2bin_matrix(svg_path_input)
+        results = []
+        try:
+            map_all = (cv2.resize(np.float32(map_all), matrix_resize_w_h) > 0) * 1  # (width, height)
+            results.append(json.dumps(map_all.tolist()))  # narray/list to string
+            results.append("good")
+            results.append(str(matrix_resize_w_h))
+
+        except:
+            results.append(map_all)  # skipped
+            results.append(svg_path_input)  # skipped
+            results.append(str(matrix_resize_w_h))
+
+        img_paths = session.get('img_path', None)
+        ml_model = model.ModelClass()
+        model_path = session.get('model_path', None)
+        ml_model.load_model(model_path)
+
+        image = model.load_image(img_paths[0])
+        image_tensor = model.process_image(image)
+        image_tensor = model.masking(image_tensor, map_all)
+        print(image_tensor.shape)
+        image_name = img_paths[0].split('/')[-1]
+        print(image_name)
+        anno_path = './static/images/annotated/annotated_' + image_name
+        model.save_image(image, map_all, anno_path)
+
+        # print(image_tensor.shape)
+        result = ml_model.predict(image_tensor)
+
+    return render_template('result.html', title='Result', task_title='test', result=result, img_path=img_paths[0], draw_display='', anno_path=anno_path)
+
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=False)
--- model.py
+++ model.py
@@ -1,48 +1,117 @@
-import cv2
+from PIL import Image
 import pandas as pd
 import numpy as np
+import torch
+from torchvision import transforms, models
+import urllib
+from PIL import Image
+import os
 
 
-df = pd.read_csv('./output/done/results_img_gender_chunk_1_factual_20240215_164304_29.csv', index_col=0)
+class ModelClass():
+    def __init__(self):
+        self.model = None
 
-attention = df['attention'][0]
+    def load_model(self, model_path):
+        if model_path is None:
+            self.model = models.resnet18(pretrained=True)
+            num_ftrs = self.model.fc.in_features
+            self.model.fc = torch.nn.Linear(num_ftrs, 2)
+        else:
+            if os.path.isfile(model_path):
+                self.model = torch.load(model_path)
+            else:
+                raise ValueError('Model does not exist')
 
-string_list = attention.strip('[]')
+    def predict(self, input_batch):
+        self.model.eval()
+        if torch.cuda.is_available():
+            input_batch = input_batch.to('cuda')
+            self.model.to('cuda')
 
-# Split the string into individual sublist strings
-sublists = string_list.split('], ')
+        with torch.no_grad():
+            output = self.model(input_batch)
+        probabilities = torch.nn.functional.softmax(output[0], dim=0)
 
-# Initialize an empty list to store the result
-list_result = []
-
-# Iterate over each sublist string
-for sublist_str in sublists:
-    # Remove any remaining brackets
-    sublist_str = sublist_str.strip('[]')
-    # Split the sublist string into individual elements
-    sublist_elements = sublist_str.split(', ')
-    # Convert elements to integers if necessary
-    sublist = [int(item) for item in sublist_elements]
-    # Append the sublist to the result list
-    list_result.append(sublist)
-
-attention = np.array(list_result)
-print(attention.shape)
+        return probabilities
 
 
-# Scale the array to the range of 0-255
-grayscale_array = attention * 255
+def string_to_array(text: str):
+    string_list = text.strip('[]')
 
-# Convert to uint8 datatype
-grayscale_array = grayscale_array.astype(np.uint8)
+    # Split the string into individual sublist strings
+    sublists = string_list.split('], ')
 
-# Set a threshold value (adjust as needed)
-threshold_value = 128
+    # Initialize an empty list to store the result
+    list_result = []
 
-# Apply thresholding
-_, black_white_image = cv2.threshold(grayscale_array, threshold_value, 255, cv2.THRESH_BINARY)
+    # Iterate over each sublist string
+    for sublist_str in sublists:
+        # Remove any remaining brackets
+        sublist_str = sublist_str.strip('[]')
+        # Split the sublist string into individual elements
+        sublist_elements = sublist_str.split(', ')
+        # Convert elements to integers if necessary
+        sublist = [int(item) for item in sublist_elements]
+        # Append the sublist to the result list
+        list_result.append(sublist)
 
-# Display or save the image
-cv2.imshow('Black and White Image', black_white_image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    return np.array(list_result)
+
+
+def load_image(img_path: str):
+    image = Image.open(img_path)
+    return image
+
+
+def process_image(image):
+    # Define transformations to be applied to the image
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),  # Resize image to match ResNet input size
+        transforms.ToTensor(),  # Convert image to PyTorch tensor
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # Normalize the image
+    ])
+
+    # Apply the transformations
+    image_tensor = transform(image)
+    # Add a batch dimension
+    image_tensor = image_tensor.unsqueeze(0)
+
+    # Now, image_tensor is ready to be sent into the ResNet model
+    # print(image_tensor.shape)  # Should print: torch.Size([1, 3, 224, 224])
+    return image_tensor
+
+
+def masking(image, mask):
+    bool_mask = mask.astype(bool)
+    image[:, :, ~bool_mask] = 0
+    return image
+
+def save_image(image, mask, path):
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),  # Resize image to match ResNet input size
+    ])
+
+    # Apply the transformations
+    image = np.array(transform(image))
+    print(image.shape)
+    bool_mask = mask.astype(bool)
+    image[~bool_mask, :] = 0
+    # image = image.squeeze(0)
+    print(image.shape)
+    image = Image.fromarray(image)
+    image.save(path)
+
+
+if __name__ == '__main__':
+    img = load_image('./static/images/img_gender/demo_img_1.jpg')
+
+    ra = np.random.random((3, 3))
+    mask = np.random.randint(2, size=(3, 3))
+
+    print(mask)
+    print(ra)
+    mask = mask.astype(bool)
+    ra[~mask] = 0
+
+    print(ra)
