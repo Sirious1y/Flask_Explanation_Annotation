8ad30399837a770ed496eb0d4b1c7f29a4c3b010
Sirious1y <jamess0378@gmail.com>
2024-10-23 20:38:53 -0400

    Update api.py

--- api.py
+++ api.py
@@ -31,5 +31,5 @@ def predict():
     return jsonify({'result': result, 'masked': masked_image.tolist()})
 
 if __name__ == '__main__': 
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5001)
 
